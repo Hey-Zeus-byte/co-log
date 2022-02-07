a6afb5fc6e15bd03e6e7cf4da26dbd4bd77032fa
Hey-Zeus-byte <heyzeus.1096@gmail.com>
2022-02-07 10:50:52 -0800

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+//import ReactDOM from "react-dom"
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+export default App;
+// const rootElement = document.getElementById("root")
+// ReactDOM.render(<App />, rootElement)
+
